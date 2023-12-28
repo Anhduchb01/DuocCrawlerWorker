2fb6bc75dabcf6caf9585fc7e83c3d2be7601a69
Anhduchb01 <anhducythb01@gmail.com>
2023-12-28 12:06:43 +0700

    Update

--- crawler/__init__.py
+++ crawler/__init__.py
@@ -460,12 +460,9 @@ def run_spider_crawl(spider,config_crawl,addressPage):
 	print('setting TELNETCONSOLE_ENABLED',setting.copy_to_dict()['TELNETCONSOLE_ENABLED'])
 	configure_logging()
 	crawl_runner = CrawlerRunner(setting)
-	reactor.run()
 	eventual = crawl_runner.crawl(
 		spider,config = config_crawl)
 	# When the crawling is finished, stop the reactor
-	eventual.addBoth(lambda _: reactor.stop())
-
 @celery.task(name='crawl_new')
 def crawl_new(namePage,industry):
 	crawler_info = db.crawlers.find_one({'addressPage': namePage,'industry':industry})
